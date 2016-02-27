2e7bdc580dc74dca3cbb9a5d75029f4a1d94ec47
mseryn <melanie.e.cornelius@gmail.com>
2016-02-27 14:27:42 -0600

    implemented and fixed meeting class

---
+++ src/schedule/task/meeting.py
@@ -0,0 +1,40 @@
+import datetime
+
+
+def meeting():
+    def __init__(self, name = "default meeting name", time = Null, room = Null, participants = []):
+        _name = name
+        _time = time
+        _room = room
+        _participants = participants
+
+    def get_time(self):
+        return _time
+
+    def get_name(self):
+        return _name
+
+    def get_room(self):
+        return _room
+
+    def get_participants(self):
+        return _participants
+
+    def set_time(name):
+        self._name = name
+
+    def set_time(time):
+        if time is instanceof(datetime.datetime):
+            self._time = time
+        else:
+            print("time must be a datetime.datetime object")
+
+    def set_room(room):
+        self._room = room
+
+    def set_participants(participants):
+        print("WARNING: setting participants erases former participants list")
+        self._participants = participants
+
+    def add_participant(participant):
+        self._participants.append(participant)
