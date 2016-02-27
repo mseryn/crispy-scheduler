7f0515093898487c9928bcc6ea3656e3f9eec807
mseryn <melanie.e.cornelius@gmail.com>
2016-02-27 14:35:00 -0600

    implemented person

---
+++ src/schedule/resource/person.py
@@ -0,0 +1,22 @@
+def person():
+    def __init__(self, name = "default person name", tasks = []):
+        _name = name
+        _tasks = tasks
+
+    def get_name(self):
+        return self._name
+
+    def get_tasks(self):
+        return self._tasks
+
+    def set_name(self, name):
+        self._name = name
+
+    def overwrite_tasks(self, tasks):
+        self._tasks = tasks
+
+    def clear_tasks(self):
+        self._tasks = []
+
+    def add_task(self, task):
+        self._tasks.append(task)
